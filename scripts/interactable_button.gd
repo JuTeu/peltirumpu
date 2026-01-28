f5ca161c2dfd8329cb6c4c039fea54b6f5bc394b
Juho Teulahti <juho.teulahti@wpk.tpu.fi>
2026-01-28 16:29:03 +0200

    Nappi animaatio

--- scripts/interactable_button.gd
+++ scripts/interactable_button.gd
@@ -1,11 +1,15 @@
 class_name InteractableButton extends Interactable
 
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
+
 signal pressed()
 
 func _ready() -> void:
 	should_lock_camera = false
 
 func start_interaction() -> void:
+	_animation_player.play("RESET")
+	_animation_player.play("pressed")
 	pressed.emit()
 
 func end_interaction() -> void:
