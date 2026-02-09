4ec8313213c552f3690fc387198e6666cae18e84
Juho Teulahti <juho.teulahti@wpk.tpu.fi>
2026-02-09 14:51:09 +0200

    Screen shut down and turn on animation

--- scripts/screen.gd
+++ scripts/screen.gd
@@ -5,18 +5,28 @@ var powered: bool:
 		return _powered
 	set(value):
 		_powered = value
+		if _tween:
+			_tween.kill()
+		_tween = create_tween()
 		if value:
-			_material.albedo_color = Color.WHITE
+			_tween.tween_method(_change_screen_status_progress, _material.get_shader_parameter("progress"), 0.0, 1.0)
+			#_material.albedo_color = Color.WHITE
 		else:
-			_material.albedo_color = Color.BLACK
+			#_material.albedo_color = Color.BLACK
+			_tween.tween_method(_change_screen_status_progress, _material.get_shader_parameter("progress"), 1.0, 1.0)
 var _powered := true
+var _tween: Tween
 @onready var _kaiju_sub_viewport: SubViewport = $"../../KaijuSubViewport"
-@onready var _material: StandardMaterial3D = get_surface_override_material(0)
+@onready var _material: ShaderMaterial = get_surface_override_material(0)
 var _timer := 0.0
+
+func _change_screen_status_progress(progress: float) -> void:
+	_material.set_shader_parameter("progress", progress)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_material.albedo_texture = _kaiju_sub_viewport.get_texture()
+	_material.set_shader_parameter("tex", _kaiju_sub_viewport.get_texture())
+	#_material.albedo_texture = _kaiju_sub_viewport.get_texture()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
